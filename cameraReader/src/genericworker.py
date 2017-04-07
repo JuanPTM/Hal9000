ce53dc83a6b0bdafc663a3c2b5119622b678a04c
JuanP Torres <JuanP.95.torres@gmail.com>
2017-04-07 13:21:40 +0200

    Initial commit

---
+++ cameraReader/src/genericworker.py
@@ -0,0 +1,70 @@
+#
+# Copyright (C) 2017 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, Ice, os
+from PySide import *
+
+ROBOCOMP = ''
+try:
+	ROBOCOMP = os.environ['ROBOCOMP']
+except KeyError:
+	print '$ROBOCOMP environment variable not set, using the default value /opt/robocomp'
+	ROBOCOMP = '/opt/robocomp'
+
+preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ --all /opt/robocomp/interfaces/"
+Ice.loadSlice(preStr+"CommonBehavior.ice")
+import RoboCompCommonBehavior
+
+preStr = "-I/opt/robocomp/interfaces/ -I"+ROBOCOMP+"/interfaces/ --all /opt/robocomp/interfaces/"
+Ice.loadSlice(preStr+"peopleTracker3d.ice")
+from peopleTracker3dMod import *
+
+
+
+
+class GenericWorker(QtCore.QObject):
+	kill = QtCore.Signal()
+
+
+	def __init__(self, mprx):
+		super(GenericWorker, self).__init__()
+
+
+		self.peopletrackerCam22_proxy = mprx["peopletracker3dProxy1"]
+		self.peopletrackerCam23_proxy = mprx["peopletracker3dProxy2"]
+		self.peopletrackerCam24_proxy = mprx["peopletracker3dProxy3"]
+		self.peopletrackerCam25_proxy = mprx["peopletracker3dProxy4"]
+		
+		
+		self.mutex = QtCore.QMutex(QtCore.QMutex.Recursive)
+		self.Period = 30
+		self.timer = QtCore.QTimer(self)
+
+
+	@QtCore.Slot()
+	def killYourSelf(self):
+		rDebug("Killing myself")
+		self.kill.emit()
+
+	# \brief Change compute period
+	# @param per Period in ms
+	@QtCore.Slot(int)
+	def setPeriod(self, p):
+		print "Period changed", p
+		Period = p
+		timer.start(Period)
